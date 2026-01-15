36da75f72d3125a0087efda023cc104c9fea49d6
ar4rpon <hsgw28tech@gmail.com>
2026-01-15 18:10:17 +0900

    ci: add pre-commit type check and pre-push test hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
-  "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
-  "*.{json,md,yml,yaml}": ["prettier --write"],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{json,md,yml,yaml}': ['prettier --write'],
+  '*.{js,cjs,mjs}': ['eslint --fix', 'prettier --write'],
 };
